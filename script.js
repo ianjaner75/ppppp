dbd8caff3cc42e394f1137f7bc198d9ed8a99e3c
ianjaner75 <ianjaner55@gmail.com>
2024-10-08 11:17:02 -0500

    pruebas

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('moreInfoBtn').addEventListener('click', function() {
+    const moreInfo = document.getElementById('moreInfo');
+    if (moreInfo.style.display === 'none') {
+        moreInfo.style.display = 'block';
+    } else {
+        moreInfo.style.display = 'none';
+    }
+});
